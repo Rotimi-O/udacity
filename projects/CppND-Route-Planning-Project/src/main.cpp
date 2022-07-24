9934a95c0c0e24f54b977e49ab0911da43014116
Otasowie Osifo <Otasowie.Rotimi.Ebose.Osifo@gmail.com>
2022-07-24 17:52:03 +0200

    update main.cpp

--- projects/CppND-Route-Planning-Project/src/main.cpp
+++ projects/CppND-Route-Planning-Project/src/main.cpp
@@ -58,10 +58,7 @@ int main(int argc, const char **argv)
     float end_x = 90.0;
     float end_y = 90.0;
     std::cin>>start_x >>start_y >>end_x >>end_y;
-    // TODO 1: Declare floats `start_x`, `start_y`, `end_x`, and `end_y` and get
-    // user input for these values using std::cin. Pass the user input to the
-    // RoutePlanner object below in place of 10, 10, 90, 90.
-
+    
     // Build Model.
     RouteModel model{osm_data};
 
