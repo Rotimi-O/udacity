52abbc28f5e127ee1391a9fb9945ffecaa6c3e4f
Otasowie Osifo <Otasowie.Rotimi.Ebose.Osifo@gmail.com>
2022-07-24 17:34:23 +0200

    implemented the task in main

--- projects/CppND-Route-Planning-Project/src/main.cpp
+++ projects/CppND-Route-Planning-Project/src/main.cpp
@@ -51,7 +51,13 @@ int main(int argc, const char **argv)
         else
             osm_data = std::move(*data);
     }
-    
+
+    std::cout<< "enter floating point values for: start_xcoordinate, start_ycoordinate, end_xcoordinate and end_ycoordinate:  ";
+    float start_x = 10.0;
+    float start_y = 10.0;
+    float end_x = 90.0;
+    float end_y = 90.0;
+    std::cin>>start_x >>start_y >>end_x >>end_y;
     // TODO 1: Declare floats `start_x`, `start_y`, `end_x`, and `end_y` and get
     // user input for these values using std::cin. Pass the user input to the
     // RoutePlanner object below in place of 10, 10, 90, 90.
@@ -60,7 +66,8 @@ int main(int argc, const char **argv)
     RouteModel model{osm_data};
 
     // Create RoutePlanner object and perform A* search.
-    RoutePlanner route_planner{model, 10, 10, 90, 90};
+    //RoutePlanner route_planner{model, 10, 10, 90, 90};
+    RoutePlanner route_planner{model, start_x, start_y, end_x, end_y};
     route_planner.AStarSearch();
 
     std::cout << "Distance: " << route_planner.GetDistance() << " meters. \n";
