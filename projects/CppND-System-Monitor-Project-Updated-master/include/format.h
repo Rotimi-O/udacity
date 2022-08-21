5259f6a40a9349a3d1b18ed310b023695216163e
Rotimi-O <105131953+Rotimi-O@users.noreply.github.com>
2022-08-21 17:15:10 +0200

    experimented with namespaces

--- projects/CppND-System-Monitor-Project-Updated-master/include/format.h
+++ projects/CppND-System-Monitor-Project-Updated-master/include/format.h
@@ -5,25 +5,6 @@
 
 namespace Format {
 std::string ElapsedTime(long times);
-
-class TimeFormater {
- public:
-  std::string FormatedTime();
-
-  TimeFormater() {}
-  TimeFormater(long times) { 
-    init(times);
-  }
- private:
-  std::string GetHoursAsHH();
-  std::string GetMinutesAsMM();
-  std::string GetSecondsAsSS();
-
-  void init(long times);
-  long hours{0};
-  long minutes{0};
-  long seconds{0};
-};
 };  // namespace Format
 
 #endif
--- projects/CppND-System-Monitor-Project-Updated-master/src/format.cpp
+++ projects/CppND-System-Monitor-Project-Updated-master/src/format.cpp
@@ -1,61 +1,72 @@
-#include <string>
-
 #include "format.h"
+
+#include <string>
 
 using std::string;
 
+namespace {
+class TimeFormater {
+ public:
+  TimeFormater() {}
+  TimeFormater(long times) { init(times); }
+
+  std::string FormatedTime() {
+    std::string hh = GetHoursAsHH();
+    std::string mm = GetMinutesAsMM();
+    std::string ss = GetSecondsAsSS();
+
+    return hh + ":" + mm + ":" + ss;
+  }
+
+ private:
+  std::string GetHoursAsHH() {
+    std::string hh = "00";
+    if (hours >= 10) {
+      hh = std::to_string(hours);
+    } else if (hours >= 0 && hours < 10) {
+      hh = "0" + std::to_string(hours);
+    }
+    return hh;
+  }
+
+  std::string GetMinutesAsMM() {
+    std::string mm = "00";
+    if (minutes >= 10) {
+      mm = std::to_string(minutes);
+    } else if (minutes >= 0 && minutes < 10) {
+      mm = "0" + std::to_string(minutes);
+    }
+    return mm;
+  }
+
+  std::string GetSecondsAsSS() {
+    std::string ss = "00";
+    if (seconds >= 10) {
+      ss = std::to_string(seconds);
+    } else if (seconds >= 0 && seconds < 10) {
+      ss = "0" + std::to_string(seconds);
+    }
+    return ss;
+  }
+
+  void init(long seconds) {
+    long seconds_in_one_hour = 3600;
+    hours = seconds / seconds_in_one_hour;
+    long remainder = seconds % seconds_in_one_hour;
+    minutes = remainder / 60;
+    seconds = remainder % 60;
+  }
+
+  long hours{0};
+  long minutes{0};
+  long seconds{0};
+};
+};  // namespace
 // TODO: Complete this helper function
 // INPUT: Long int measuring seconds
 // OUTPUT: HH:MM:SS
 // REMOVE: [[maybe_unused]] once you define the function
-string Format::ElapsedTime(long seconds) { 
-    Format::TimeFormater timeFormater{seconds};
-    return timeFormater.FormatedTime(); 
-    }
-
-     std::string Format::TimeFormater::GetHoursAsHH() {
-       std::string hh = "00";
-       if (hours >= 10) {
-            hh = std::to_string(hours);
-        } else if(hours >= 0 && hours < 10) {
-           hh = "0" + std::to_string(hours);
-        }
-        return hh;
-    }
-
-     std::string Format::TimeFormater::GetMinutesAsMM() {
-       std::string mm = "00";
-       if(minutes >= 10) {
-        mm = std::to_string(minutes);
-       }else if(minutes >= 0 && minutes < 10) {
-           mm = "0" + std::to_string(minutes);
-        }
-       return mm;
-     }
-
-     std::string Format::TimeFormater::GetSecondsAsSS() {
-       std::string ss = "00";
-       if(seconds >= 10) {
-        ss = std::to_string(seconds);
-       }else if(seconds >= 0 && seconds < 10) {
-           ss = "0" + std::to_string(seconds);
-        }
-       return ss;
-     }
-
-     void Format::TimeFormater::init(long seconds) {
-       long seconds_in_one_hour = 3600;
-       hours = seconds / seconds_in_one_hour;
-       long remainder = seconds % seconds_in_one_hour;
-       minutes = remainder / 60;
-       seconds = remainder % 60;
-     }
-
-     std::string Format::TimeFormater::FormatedTime() {
-        std::string hh = GetHoursAsHH();
-        std::string mm = GetMinutesAsMM();
-        std::string ss = GetSecondsAsSS();
-
-        return hh + ":" + mm + ":" + ss;
-     }
-     
+string Format::ElapsedTime(long seconds) {
+  TimeFormater timeFormater{seconds};
+  return timeFormater.FormatedTime();
+}
