1520627c33fe5d9d1e03419899c6063ad599976f
Rotimi-O <105131953+Rotimi-O@users.noreply.github.com>
2022-09-17 18:38:23 +0200

    implemented the methods in process.cpp

--- projects/CppND-System-Monitor-Project-Updated-master/src/process.cpp
+++ projects/CppND-System-Monitor-Project-Updated-master/src/process.cpp
@@ -4,30 +4,48 @@
 #include <string>
 #include <vector>
 
+#include "linux_parser.h"
 #include "process.h"
 
 using std::string;
 using std::to_string;
 using std::vector;
 
-// TODO: Return this process's ID
-int Process::Pid() { return 0; }
 
-// TODO: Return this process's CPU utilization
-float Process::CpuUtilization() { return 0; }
+int Process::Pid() { return this->pid_; }
 
-// TODO: Return the command that generated this process
-string Process::Command() { return string(); }
 
-// TODO: Return this process's memory utilization
-string Process::Ram() { return string(); }
+float Process::CpuUtilization() {
+	float up_time = LinuxParser::UpTime(pid_); //conversion from long to float
+	float active_jiffies = LinuxParser::ActiveJiffies(pid_); //conversion from long to float
 
-// TODO: Return the user (name) that generated this process
-string Process::User() { return string(); }
+	this->cpu_utilization_ = (active_jiffies / up_time);
+	return this->cpu_utilization_;
+}
 
-// TODO: Return the age of this process (in seconds)
-long int Process::UpTime() { return 0; }
+std::string Process::Command() {
+	this->command_ = LinuxParser::Command(pid_);
+	return this->command_;
+}
 
-// TODO: Overload the "less than" comparison operator for Process objects
-// REMOVE: [[maybe_unused]] once you define the function
-bool Process::operator<(Process const& a[[maybe_unused]]) const { return true; }
+std::string Process::Ram() {
+	this->ram_ =  LinuxParser::Ram(pid_);
+	return this->ram_;
+}
+
+
+std::string Process::User() {
+	this->user_ = LinuxParser::User(pid_);
+	return this->user_;
+}
+
+
+long int Process::UpTime() {
+	this->up_time_ = LinuxParser::UpTime(pid_);
+	return this->up_time_;
+}
+
+
+bool Process::operator<(Process const& a) const {
+	return ((this->up_time_) < (a.up_time_));
+}
